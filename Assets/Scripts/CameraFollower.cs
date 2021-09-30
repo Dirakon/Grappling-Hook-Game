249f32f9646058c436b3143c477780a3d986ff2f
Dirakon <609700zxc@mail.ru>
2021-09-30 20:05:52 +0300

    improved camera (i think)

--- Assets/Scripts/CameraFollower.cs
+++ Assets/Scripts/CameraFollower.cs
@@ -17,7 +17,8 @@ public class CameraFollower : MonoBehaviour
     // Start is called before the first frame update
     void Awake(){
         standartSize = desiredSize;
-        Camera.main.orthographicSize = standartSize* Screen.height / Screen.width * 0.5f;
+        float aspectRatio = Mathf.Min(Screen.width/(float)Screen.height,Screen.height/(float)Screen.width);
+        Camera.main.orthographicSize = standartSize/aspectRatio;
     }
     void Start()
     {
@@ -29,15 +30,16 @@ public class CameraFollower : MonoBehaviour
     {
         Vector3 desiredPosition = (whoToFollow.objectToRotateAround == null?
         whoToFollow.transform : whoToFollow.objectToRotateAround ).position +offset; 
+        float aspectRatio = Mathf.Min(Screen.width/(float)Screen.height,Screen.height/(float)Screen.width);
         desiredSize  = (whoToFollow.objectToRotateAround == null? standartSize :Mathf.Max(standartSize, whoToFollow.currentRotatingDistance*3));
-        float currentSize = camera.orthographicSize/ Screen.height * Screen.width / 0.5f;
+        float currentSize = camera.orthographicSize*aspectRatio;
         float sign = Mathf.Sign(desiredSize-currentSize);
         currentSize += Time.deltaTime*zoomSpeed*sign;
         if (Mathf.Sign(desiredSize-currentSize) != sign){
             // Overshot a bit
             currentSize = desiredSize;
         }
-        Camera.main.orthographicSize = currentSize * Screen.height / Screen.width * 0.5f;
+        Camera.main.orthographicSize = currentSize /aspectRatio;
         transform.position = Vector3.MoveTowards(transform.position,desiredPosition,Time.deltaTime*speed);
     }
 }
